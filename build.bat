4fd1b2da893b639aaf80adbd4271ac25f871226e
VexilophragmaticZynglethorpean <187556078+VexilophragmaticZynglethorpean@users.noreply.github.com>
2025-09-17 22:01:45 +0545

    Fix build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo off
+
+set VCPKG_DEFAULT_TRIPLET=x64-mingw
+set BUILD_TYPE=release
+
 IF NOT EXIST vcpkg (
     git clone https://github.com/microsoft/vcpkg.git vcpkg
     call vcpkg\bootstrap-vcpkg.bat
@@ -7,5 +11,5 @@ IF NOT EXIST vcpkg (
 IF EXIST build\CMakeCache.txt del build\CMakeCache.txt
 IF EXIST build\CMakeFiles rmdir /s /q build\CMakeFiles
 
-cmake --preset vcpkg-release
-cmake --build build --preset build-release
+cmake --preset vcpkg-%BUILD_TYPE%
+cmake --build build --preset build-%BUILD_TYPE%
--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+ 
+export VCPKG_DEFAULT_TRIPLET=x64-linux
+export BUILD_TYPE=release
+
 if [ ! -d "./vcpkg" ]; then
     git clone https://github.com/microsoft/vcpkg.git ./vcpkg
     ./vcpkg/bootstrap-vcpkg.sh
@@ -5,5 +10,5 @@ fi
 [ -f build/CMakeCache.txt ] && rm build/CMakeCache.txt
 [ -d build/CMakeFiles ] && rm -rf build/CMakeFiles
 
-cmake --preset vcpkg-release
-cmake --build build --preset build-release
+cmake --preset vcpkg-$BUILD_TYPE
+cmake --build build --preset build-$BUILD_TYPE
