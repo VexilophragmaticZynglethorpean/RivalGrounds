f62c56d2dde8df2ad888cedf186f536e9abb37e9
VexilophragmaticZynglethorpean <187556078+VexilophragmaticZynglethorpean@users.noreply.github.com>
2025-09-17 19:26:29 +0545

    Modify build.sh + related changes

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
-cmake --preset vcpkg
+if [ ! -d "./vcpkg" ]; then
+    git clone https://github.com/microsoft/vcpkg.git ./vcpkg
+    ./vcpkg/bootstrap-vcpkg.sh
+    ./vcpkg/vcpkg install
+fi
+rm -rf build
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build
